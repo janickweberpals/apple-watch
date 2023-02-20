52c4d96ba4ee553654eb5520d345bb61b9a954cf
Janick Weberpals <janick.weberpals@me.com>
2023-02-20 11:20:07 -0500

    modified function

--- functions/read_workouts.R
+++ functions/read_workouts.R
@@ -2,13 +2,11 @@
 # adapted from https://gist.github.com/ryanpraski/ba9baee2583cfb1af88ca4ec62311a3d
 load_workouts <- function(){
   
-  suppressPackageStartupMessages(library(tidyverse))
-  
   # load static file that will be appended
   workouts <- readr::read_csv(here::here("data", "workouts_master.csv"), show_col_types = FALSE)
   
   # load new measurements
-  tmp <- readr::read_csv(here::here("data", "allWorkouts.csv"), show_col_types = FALSE)
+s
   
   # append and save
   workouts_appended <- dplyr::bind_rows(workouts, tmp) %>% 
@@ -25,12 +23,12 @@ load_workouts <- function(){
       year = lubridate::year(start),
       month = lubridate::month(start),
       day = lubridate::wday(start, label = T),
-      activityLengthMin = round(as.numeric(end - start), digits = 2)
+      activity_length_min = round(as.numeric(end - start), digits = 2)
     ) %>% 
     dplyr::rename(
       activity = type,
-      startDate = start,
-      endDate = end
+      start_date = start,
+      end_date = end
     )
   
   
